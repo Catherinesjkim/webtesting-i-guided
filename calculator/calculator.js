8f800572d482eb1b20ec548dbbb53efbc2689c21
Catherine-Kim <catherine@ipwe.com>
2020-06-07 16:34:48 -0700

    Add Updated Files Because: - Learned the basics of automated testing and used jest to write unit tests.
    This commit:
    - Followed along the instructor using the Test Driven Development (TDD) to implement the add() feature of a simple calculator.

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,6 +2,12 @@ module.exports = {
   add,
 };
 
-function add() {
-  return null;
+// pure function
+function add(a = 0, b = 0) {
+  return a + b
 }
+
+// start with low hanging fruit --> happy path (your basic assumptions or expectations)
+// then check for in invalid input
+// then move on to edge cases - numbers in JS have a limit
+// then rare edge cases - rarely there
